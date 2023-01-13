18453da03b57b72aba25765f9d7a55721f031d0e
PPS <pavlina.psarsky@gmail.com>
2023-01-13 19:43:06 +0100

    working

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const fileName = document.getElementById("fileName");
 const tagName = document.getElementById("tagName");
 const form = document.getElementById("form");
 const tableBody = document.getElementById("tableBody");
-let valuesObject = {};
+const resetTag = document.getElementById("reset-tag");
+const resetFilename = document.getElementById("reset-filename");
 
-async function fetchValues(name1, name2) {
+const fetchValues = async (name1, name2) => {
   let tagValuesArray = [];
-
   try {
     const response = await fetch(name1);
     data = await response.text();
@@ -14,29 +14,21 @@ async function fetchValues(name1, name2) {
     const xmlDOM = parser.parseFromString(data, "text/xml");
     const entries = xmlDOM.querySelectorAll("book");
     entries.forEach((entry) => {
-      console.log(entry.getElementsByTagName(name2)[0].innerHTML);
       let tagValue = entry.getElementsByTagName(name2)[0].innerHTML;
       tagValuesArray.push(tagValue);
     });
   } catch (err) {
     console.log(err);
   }
-  organizeValues(tagValuesArray);
-  displayValues(valuesObject);
-}
 
-function organizeValues(array) {
-  valuesObject = array.reduce((acc, cur) => {
+  let valuesObject = tagValuesArray.reduce((acc, cur) => {
     return {
       ...acc,
       [cur]: Number(acc[cur] || []) + 1,
     };
   }, {});
-}
 
-const displayValues = (object) => {
-  console.log(object);
-  for (const [key, value] of Object.entries(object)) {
+  for (const [key, value] of Object.entries(valuesObject)) {
     let row = `<tr>
                 <td>${key}</td>
                 <td>${value}</td>
@@ -45,6 +37,7 @@ const displayValues = (object) => {
   }
 };
 
-form.addEventListener("submit", () => {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   fetchValues(fileName.value, tagName.value);
 });
